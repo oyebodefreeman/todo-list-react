7ebf8c4fc1dbe2a960b1bbdfdecd938a8b4a28eb
oyebodefreeman <bodokunfolami@gmail.com>
2019-02-18 14:07:47 -0800

    compontent updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './app.css'
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,7 +11,9 @@ class App extends Component {
       items: [],
     }
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleCheckChange = this.handleCheckChange.bind(this)
     this.handleInputSubmit = this.handleInputSubmit.bind(this)
+    this.clearTodoList = this.clearTodoList.bind(this)
   }
 
   handleInputChange(e) {
@@ -19,40 +22,143 @@ class App extends Component {
     })
   }
 
+  handleCheckChange(e) {
+    let span = e.target.parentNode.parentNode.childNodes[1];
+    const todoList = this.getTodoList()
+
+    if (e.target.checked) {
+      span.style.textDecoration = "line-through";
+      todoList.map((item, index) => {
+        if (span.innerHTML === item.text)
+          item.completed = true
+      })
+      // Store update
+      localStorage.setItem("to-do", JSON.stringify(todoList))
+
+    } else {
+      span.style.textDecoration = "none";
+      todoList.map((item, index) => {
+        if (span.innerHTML === item.text)
+          item.completed = false
+      })
+      // Store update
+      localStorage.setItem("to-do", JSON.stringify(todoList))
+
+    }
+  }
+
+  getTodoList() {
+    const todoList = localStorage.getItem('to-do');
+    if (!todoList) {
+      return []
+    } else {
+      return JSON.parse(todoList)
+    }
+  }
+
+  storeTodoList(newItem) {
+    const todoList = this.getTodoList()
+    todoList.push(newItem)
+    localStorage.setItem("to-do", JSON.stringify(todoList))
+  }
+
   handleInputSubmit(e) {
-    event.preventDefault();
+    e.preventDefault();
     const newItem = {
       text: this.state.text,
-      id: Date.now()
+      id: Date.now(),
+      completed: false
+    }
+    if (newItem.text) {
+      // Store to-do in localStorage
+      this.storeTodoList(newItem)
+      // Show newly added items
+      this.setState(prevState => ({
+        items: prevState.items.concat(newItem),
+        text: ''
+      }))
     }
 
-    // Reset input field
-    this.setState(prevState => ({
-      items: prevState.items.concat(newItem),
-      text: ''
-    }))
+  }
 
+  clearTodoList() {
+    localStorage.removeItem("to-do")
+    const todoList = this.getTodoList()
+    this.setState({
+      items: todoList
+    })
+  }
+
+  componentDidMount() {
+    const todoList = this.getTodoList()
+    this.setState({
+      items: todoList
+    })
   }
 
   render() {
     const items = this.state.items
     return (
-      <div>
         <TodoList
           handleInputChange={this.handleInputChange}
+          handleCheckChange={this.handleCheckChange}
           handleInputSubmit={this.handleInputSubmit}
           text={this.state.text}
           items={items}
+          clearTodoList={this.clearTodoList}
         />
-      </div>
     );
   }
 }
 
 const TodoList = (props) => (
   <div className="container">
+    <h2 className="teal-text center">To-do List</h2>
     <div className="row">
-
+      <div className="col s12 m8 offset-m2">
+          <form onSubmit={props.handleInputSubmit}>
+            <div className="input-field">
+              <i className="material-icons prefix">create</i>
+              <input
+                type="text"
+                placeholder="Add new to-do"
+                value={props.text}
+                onChange={props.handleInputChange}
+                autoFocus
+              />
+            </div>
+          </form>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col s12 m8 offset-m2">
+        <button className="btn waves-effect red" onClick={props.clearTodoList}>
+          <i className="material-icons left">clear</i>
+          Clear List
+        </button>
+        <ul id="todo-list">
+          {props.items.map((item) => {
+            return (
+              <li key={item.id}>
+                <div className="row">
+                  <label className="col s2">
+                    <input
+                      type="checkbox"
+                      defaultChecked={item.completed ? true : false}
+                      className="filled-in"
+                      onChange={props.handleCheckChange}
+                    />
+                    <span></span>
+                  </label>
+                  <span style={item.completed ? {textDecoration: "line-through"} : null} className="col s10">
+                    {item.text}
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   </div>
 );
